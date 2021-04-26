790f8a5e5be0085cbd6aebb0b60c3e47afaa8dae
Rémi_Mgt <62755660+RemiMgt@users.noreply.github.com>
2021-04-26 08:21:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -37,14 +37,17 @@ pygame.init()
 #Création fenêtre :
 fenetre_width, fenetre_height = 1500, 800
 pygame.display.set_caption("Titre jeu")
-fenetre = pygame.display.set_mode((fenetre_width, fenetre_height))#, FULLSCREEN
+fenetre = pygame.display.set_mode((fenetre_width, fenetre_height), pygame.FULLSCREEN)#, FULLSCREEN
 
 #Menu :
 background = pygame.image.load("assets/fond_ecran.jpg")
+background = pygame.transform.scale(background, (fenetre_width, fenetre_height))
 images_boutons = pygame.image.load("bouton/boutons.png")
+gif_menu = pygame.image.load("sprite/idle/perso_idle.gif")
+gif_menu = pygame.transform.scale(gif_menu, (900, 900))
 
 #Fonctions :
-def generate(lien, pos, size,surface):
+def generate_in_images(lien, pos, size,surface):
     new_image = images_boutons.subsurface(surface)
     new_image = pygame.transform.scale(new_image, size)
     new_image_rect = new_image.get_rect()
@@ -52,21 +55,29 @@ def generate(lien, pos, size,surface):
     new_image_rect.y = pos[1]
     return [new_image, new_image_rect]
 
+def generate_rect(lien, pos) :
+    new_image = pygame.image.load(lien)
+    new_image_rect = new_image.get_rect()
+    new_image_rect.x = pos[0]
+    new_image_rect.y = pos[1]
+    return [new_image, new_image_rect]
 
 #Boutons :
-bouton_play = generate("bouton/bouton_play.png", (550,150), (300,100), (220, 195, 150, 50))
-bouton_settings = generate("bouton/bouton_option.png", (550,350),(300,100)  ,(220, 135, 150, 50))
-bouton_exit = generate("bouton/bouton_exit.png", (550,550), (300, 100), (30, 15, 150, 50))
-bouton_retour = generate("bouton/bouton_retour.png", (20, 780), (300, 100), (30, 15, 150, 50))
+bouton_play = generate_in_images("bouton/bouton_play.png", (230,150), (300,105), (220, 195, 20, 50))
+bouton_settings = generate_in_images("bouton/bouton_option.png", (230,350),(300,105)  ,(220, 135, 150, 50))
+bouton_exit = generate_in_images("bouton/bouton_exit.png", (230,550), (300, 105), (42, 20, 244, 90))
+bouton_retour = generate_rect("bouton/bouton_retour.png", (30, 690))
 
 #Fonctions :
 def menu() :
     fenetre.blit(background, (0, 0))
-    fenetre.blit(bouton_exit[0], (bouton_exit[1]))
+    fenetre.blit(gif_menu, (700, -40))
+    fenetre.blit(bouton_exit[0], bouton_exit[1])
     fenetre.blit(bouton_play[0], bouton_play[1])
     fenetre.blit(bouton_settings[0], bouton_settings[1])
 
 def options():
+    fenetre.blit(background, (0, 0))
     fenetre.blit(bouton_retour[0], bouton_retour[1])
 
 def jeux():
@@ -84,36 +95,30 @@ while boucle :
     # :hover --> boutons :
     if bouton_play[1].collidepoint((x,y)) :
         bouton_play[0] = pygame.transform.scale(bouton_play[0], (320, 120))
-        bouton_play[1].x, bouton_play[1].y = 540, 140
+        bouton_play[1].x, bouton_play[1].y = 240, 140
     else:
-        bouton_play = generate("bouton/bouton_play.png", (550,150), (300,100), (220, 195, 150, 50))
-        bouton_play[1].x, bouton_play[1].y = 550, 150
+        bouton_play = generate_in_images("bouton/bouton_play.png", (230,150), (300,105), (332, 304, 249, 90))
+        bouton_play[1].x, bouton_play[1].y = 250, 150
 
     if bouton_settings[1].collidepoint((x,y)) :
         bouton_settings[0] = pygame.transform.scale(bouton_settings[0], (320,120))
-        bouton_settings[1].x, bouton_settings[1].y = 540, 340
+        bouton_settings[1].x, bouton_settings[1].y = 240, 340
     else:
-        bouton_settings = generate("bouton/bouton_option.png", (550,350),(300,100)  ,(220, 135, 150, 50))
-        bouton_settings[1].x, bouton_settings[1].y = 550, 350
+        bouton_settings = generate_in_images("bouton/bouton_option.png", (230,350),(300,105)  ,(343, 209, 240, 90))
+        bouton_settings[1].x, bouton_settings[1].y = 250, 350
 
     if bouton_exit[1].collidepoint((x,y)) :
         bouton_exit[0] = pygame.transform.scale(bouton_exit[0], (320, 120))
-        bouton_exit[1].x, bouton_exit[1].y = 540, 540
+        bouton_exit[1].x, bouton_exit[1].y = 240, 540
     else:
-        bouton_exit =  generate("bouton/bouton_exit.png", (550,550), (300, 100), (30, 15, 150, 50))
-        bouton_exit[1].x, bouton_exit[1].y = 550, 550
-
-    if bouton_retour[1].collidepoint((x,y)) :
-        bouton_retour[0] = pygame.transform.scale(bouton_retour[0], (320, 120))
-        bouton_retour[1].x, bouton_retour[1].y = 540, 540
-    else:
-        bouton_retour = generate("bouton/bouton_retour.png", (20, 780), (300, 100), (30, 15, 150, 50))
-        bouton_retour[1].x, bouton_retour[1].y = 550, 550
-        #a demain pylou
+        bouton_exit = generate_in_images("bouton/bouton_exit.png", (230,550), (300, 105), (42, 20, 244, 90))
+        bouton_exit[1].x, bouton_exit[1].y = 250, 550
 
     #Stats :
     if game.stat == "menu":
         menu()
+    if game.stat == "options" :
+        options()
     if game.stat == "game" :
         jeux()
 
@@ -127,20 +132,22 @@ while boucle :
             print("Closing Project ...")
 
         if event.type == pygame.KEYDOWN :
-
             game.keys[event.key] = True
 
         if event.type == pygame.KEYUP:
             game.keys[event.key] = False
 
         if event.type == pygame.MOUSEBUTTONDOWN :
-            if bouton_play[1].collidepoint(event.pos):
-                game.stat="game"
-                print("lancement du jeu!")
-            elif bouton_settings[1].collidepoint(event.pos):
-                game.stat="options"
-                print("voila les options")
-            elif bouton_exit[1].collidepoint(event.pos):
-                pygame.quit()
-                boucle = False
-                print("Closing Project ...")
+            if game.stat == "menu" :
+                if bouton_play[1].collidepoint(event.pos):
+                    game.stat="game"
+                    print("lancement du jeu!")
+                elif bouton_settings[1].collidepoint(event.pos):
+                    game.stat="options"
+                elif bouton_exit[1].collidepoint(event.pos):
+                    pygame.quit()
+                    boucle = False
+                    print("Closing Project ...")
+            elif game.stat == "options" :
+                if bouton_retour[1].collidepoint(event.pos) :
+                    game.stat = "menu"
