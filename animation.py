b115506d1d67f42d314ca14fd48cd9f4f10c5359
Pyloupy <78490087+Pyloupy@users.noreply.github.com>
2021-04-27 17:48:12 +0200

    Update animation.py

--- animation.py
+++ animation.py
@@ -1,16 +1,22 @@
 import pygame
 
+
 class Animation(pygame.sprite.Sprite):
 
-    def __init__(self, taille_tab, n_image, temps):
+    def __init__(self, taille_tab, n_image, temps, is_resize=False,size=None, is_stop=False, is_remove=False, group=None):
         super().__init__()
-        if taille_tab != n_image:
+        if len(taille_tab) != len(n_image):
             print("erreur le nombre dimage et de taille ne sont pas les meme")
         self.n_image = n_image
+        self.size = size
+        self.is_resize = is_resize
+        self.is_stop = is_stop
+        self.is_remove = is_remove
+        self.group = group
         self.t_b = taille_tab
         self.tab = self.load()
         self.index = [0] * len(taille_tab)
-        self.image = pygame.image.load('assets/player/'+n_image[0]+".png")
+        self.image = pygame.image.load('assets/'+n_image[0]+'/'+n_image[0]+".png")
         self.image = pygame.transform.scale(self.image, (356, 512))
         self.tab_delay = []
         for i in range(len(temps)):
@@ -22,12 +28,19 @@ class Animation(pygame.sprite.Sprite):
         self.index_delay[i] += 1
         if self.tab_delay[i][self.index_delay[i]] == "True" :
             self.index_delay[i] = 0
-            if self.index[i] == len(self.tab[i]) - 1 :
+            if self.index[i] == len(self.tab[i]) - 1:
+                if self.is_stop:
+                    if self.is_remove:
+                        self.group.remove(self)
+                    return
                 self.index[i] = 0
+
             else:
                 self.index[i] += 1
-                self.image = self.tab[i][self.index[i]]
-                self.image = pygame.transform.scale(self.image, (356, 512))
+
+            self.image = self.tab[i][self.index[i]]
+            if self.is_resize:
+                self.image = pygame.transform.scale(self.image, self.size)
 
     def load(self):
         images =[]
